552abfcf9cf512334ba232b272c182373a0553fe
Vladislav <vladislav0sidorov@gmail.com>
2024-11-28 00:00:52 +0400

    PART-1 Develop a tic-tac-toe game

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,85 @@
-import { checkWin, isFill, isRightMove } from "./board"
+import { checkWin, isFill, isRightMove } from './board'
 
 // Текущее состояние доски
 let board: string[] = []
 
 // Ссылка на html элемент для вывода сообщений
-let info = <HTMLDivElement>document.getElementById("info")
+const info = <HTMLDivElement>document.getElementById('info')
 
 // Кнопки игрового поля и их инициализация
-let buttons: HTMLButtonElement[] = []
+const buttons: HTMLButtonElement[] = []
+
 for (let i = 0; i < 9; i++) {
-    let b = <HTMLButtonElement>document.getElementById("f" + i)
-    b.onclick = function (): void { step(i) }
-    buttons.push(b)
-    let f = b.textContent
-    if (f == null) { f = "_" };
-    board.push(f)
+  const b = <HTMLButtonElement>document.getElementById('f' + i)
+  const f = b.textContent === null ? '_' : b.textContent
+
+  b.onclick = () => {
+    step(i)
+  }
+  buttons.push(b)
+  board.push(f)
 }
 
 // Начальная поизиция, используется при сбросе
-let zeroBoard = board.slice()
+const zeroBoard = board.slice()
 
 // После окончании игры - true.
 // Используется для того, что-бы не делать ходов после окончания игры
 let gameOver = false
 
 // Хранит символ, который выставляется на доску при ходе.
-let turn = "X"
+let turn = 'X'
 
 // Возвращает текущий символ и меняет его для следующего хода.
-function getTurn(): "X" | "0" {
-    if (turn == "X") {
-        turn = "0"
-        return ("X")
-    } else {
-        turn = "X"
-        return ("0")
-    }
+function getTurn(): 'X' | '0' {
+  if (turn == 'X') {
+    turn = '0'
+    return 'X'
+  } else {
+    turn = 'X'
+    return '0'
+  }
 }
 
 // Кнопка для перехода к начальной позиции
-let resetButton = <HTMLButtonElement>document.getElementById("reset")
+const resetButton = <HTMLButtonElement>document.getElementById('reset')
+
+// TODO
+// Функция  должна установить в начальное состояние переменные
+//  board, turn и gameOver, и установить в начальное состояние
+//  свойство textContent html элементов buttons и info
 const reset = function (this: GlobalEventHandlers): void {
-    // TODO
-    // Функция  должна установить в начальное состояние переменные
-    //  board, turn и gameOver, и установить в начальное состояние 
-    //  свойство textContent html элементов buttons и info
+  board = zeroBoard.slice()
+  turn = 'X'
+  gameOver = false
+  info.textContent = 'Игра началась'
+  buttons.forEach((button, i) => {
+    button.textContent = board[i]
+  })
 }
+
 resetButton.onclick = reset
 
+// TODO
+// В случае, если игра не окончена и в клетку cell возможен ход,
+//  должна выполнить ход.
+// При выполнении хода нужно заполнить соответствующие элементы
+//  массивов board и buttons, затем проверить, на закончилась ли игра
+//  и, если игра закончилась, обновить переменные gameOver и info.
 function step(cell: number): void {
-    // TODO
-    // В случае, если игра не окончена и в клетку cell возможен ход,
-    //  должна выполнить ход.
-    // При выполнении хода нужно заполнить соответствующие элементы 
-    //  массивов board и buttons, затем проверить, на закончилась ли игра 
-    //  и, если игра закончилась, обновить переменные gameOver и info.
+  if (!gameOver && isRightMove(cell, board)) {
+    const currentTurn = getTurn()
+
+    board[cell] = currentTurn
+    buttons[cell].textContent = currentTurn
+
+    const result = checkWin(board)
+    if (result !== '_') {
+      gameOver = true
+      info.textContent = `Игрок ${result} выиграл!`
+    } else if (isFill(board)) {
+      gameOver = true
+      info.textContent = 'Ничья!'
+    }
+  }
 }
